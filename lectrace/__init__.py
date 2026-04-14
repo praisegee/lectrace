553a2eff27494a2054b8146d8cd7b6fa5196b0df
PraiseGod D. Adesanmi <dayopraisegod@gmail.com>
2026-04-14 09:17:43 +0100

    init project layout and pyproject.toml

---
+++ lectrace/__init__.py
@@ -0,0 +1,9 @@
+from lectrace.renderings import text, image, video, link, plot, note, system_text, url_reference
+from lectrace.reference import Reference
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "text", "image", "video", "link", "plot", "note", "system_text",
+    "url_reference", "Reference",
+]
