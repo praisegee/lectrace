0de62aa35b094713e6c22f117fba2792549fe293
PraiseGod D. Adesanmi <dayopraisegod@gmail.com>
2026-04-25 17:48:35 +0100

    bump version to 1.1.0

--- lectrace/__init__.py
+++ lectrace/__init__.py
@@ -1,7 +1,7 @@
 from lectrace.renderings import text, image, video, link, plot, note, system_text, url_reference
 from lectrace.reference import Reference
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = [
     "text", "image", "video", "link", "plot", "note", "system_text",
