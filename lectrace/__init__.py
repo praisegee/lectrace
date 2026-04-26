df88424e223c3dec5cdc56693003c2607e3da718
PraiseGod D. Adesanmi <dayopraisegod@gmail.com>
2026-04-26 20:23:43 +0100

    add MIT license

--- lectrace/__init__.py
+++ lectrace/__init__.py
@@ -1,9 +1,25 @@
-from lectrace.renderings import text, image, video, link, plot, note, system_text, url_reference
+from lectrace.renderings import (
+    text,
+    image,
+    video,
+    link,
+    plot,
+    note,
+    system_text,
+    url_reference,
+)
 from lectrace.reference import Reference
 
 __version__ = "1.1.0"
 
 __all__ = [
-    "text", "image", "video", "link", "plot", "note", "system_text",
-    "url_reference", "Reference",
+    "text",
+    "image",
+    "video",
+    "link",
+    "plot",
+    "note",
+    "system_text",
+    "url_reference",
+    "Reference",
 ]
