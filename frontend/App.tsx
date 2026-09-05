df1c59453edbd6a04b56b97d6a4eac8ed4856680
Dayo Praise Adesanmi <dayopraisegod@gmail.com>
2026-09-05 04:29:33 +0100

    ignore type error on css import in App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { TraceApp } from "./components/TraceApp";
+// @ts-ignore: CSS is handled by the bundler at runtime.
 import "./index.css";
 
 export default function App() {
